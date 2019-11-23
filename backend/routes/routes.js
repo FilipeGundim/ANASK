c3af16c1d1d35a77d3752498f958cc84f1298979
FilipeGundim <filipegundim@outlook.com>
2019-11-23 18:16:49 -0300

    quase

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('./cors');
-const sql = require('mssql'); 
+const sql = require('mssql');
 const bodyParser = require('body-parser')
 
 const config = {
@@ -17,72 +17,109 @@ app.use(bodyParser.json())
 app.post('/criar-user', (req, res) => {
     let body = req.body
     request.query(`insert into usuario values ('${body.nome}', '${body.datanasc}', '${body.sexo}', '${body.email}', '${body.senha}');`,
-     (err, recordset) => {  
-        if (err) {
-            console.log(err)
-        }
-     res.status(200)
-    })
+        (err, recordset) => {
+            if (err) {
+                console.log(err)
+            }
+            res.status(200)
+        })
 })
 
-app.get('/atividades/:id', (req, res)=>{
+app.get('/atividades/:id', (req, res) => {
     let id = req.params.id
     request.query(`SELECT * FROM usuario_atividade WHERE id_usuario = ${id} and status = 1 ;`,
-     (err, recordset) => {  
-        if (err) {
-            console.log(err)
-        }
-     res.json(recordset) 
-    })
+        (err, recordset) => {
+            if (err) {
+                console.log(err)
+            }
+            res.json(recordset)
+        })
 })
 
-app.post('/cria-atividade', (req, res)=>{ 
+app.get('/todas-atividades/:id', (req, res) => {
+    let id = req.params.id
+    request.query(`SELECT * FROM usuario_atividade WHERE id_usuario = ${id} and status =0;`,
+        (err, recordset) => {
+            if (err) {
+                console.log(err)
+            }
+            res.json(recordset)
+        })
+})
+
+app.post('/cria-atividade', (req, res) => {
     let body = req.body
     request.query(`INSERT INTO usuario_atividade VALUES ('${body.titulo}', '${body.descricao}', '${body.data_ini}', 
     '${body.data_fim}', 1, '${body.responsavel}')`,
-    (err, recordset) => {  
-        if (err) {
-            console.log(err) 
-        }
-     
-    })
+        (err, recordset) => {
+            if (err) {
+                console.log(err)
+            }
+
+        })
     res.status(200);
 })
 
-app.post('/finaliza-atividade/:atividade', (req, res)=>{
+app.post('/finaliza-atividade/:atividade', (req, res) => {
     let atividade = req.params.atividade
     request.query(`UPDATE usuario_atividade SET status = 0  WHERE id = ${atividade}`,
-    (err, recordset) => {  
-        if (err) {
-            console.log(err)
-        }
-    })
+        (err, recordset) => {
+            if (err) {
+                console.log(err)
+            }
+        })
     res.status(200);
-})
+}) 
 
-app.post('/cria-projeto', (req, res)=>{ 
+app.post('/cria-projeto', (req, res) => {
     let body = req.body
     request.query(`INSERT INTO projeto VALUES 
     ('${body.titulo}', '${body.descricao}', '${body.data_ini}', 
     '${body.data_fim}','${body.responsavel}', 1, '${body.responsavel}')`,
-    (err, recordset) => {  
+        (err, recordset) => {
+            if (err) {
+                console.log(err)
+            }
+        })
+    res.status(200);
+})
+
+app.post('/usuario-projeto/:id/:projeto', (req, res) => {
+    let id = req.params.id
+    let projeto = req.params.id
+    request.query(`INSERT INTO usuario_projeto VALUES(${id}, ${projeto}, ${id})`, (err, recordset) => {
         if (err) {
-            console.log(err) 
-        } 
+            console.log(err)
+        }
     })
     res.status(200);
 })
 
-app.get('/projetos/:filtro', (req, res)=>{
+app.get('/projeto/:id', (req, res) => {
+    let id = req.params.id
+    request.query(`SELECT * FROM projeto AS p
+    INNER JOIN usuario_projeto AS up
+    ON p.id = up.projeto_id
+    WHERE up.usuario_id = ${id} and status = 1 ;`,
+        (err, recordset) => {
+            if (err) {
+                console.log(err)
+            }
+            res.json(recordset)
+        })
+})
+
+
+app.get('/projetos/:filtro', (req, res) => {
     let filtro = req.params.filtro
-    
+
     request.query(`SELECT * FROM projeto WHERE titulo LIKE '%${filtro}%'`,
-    (err, recordset) => {  
-        if (err) {
-            console.log(err) 
-        }
-     res.json(recordset)
-    })
+        (err, recordset) => {
+            if (err) {
+                console.log(err)
+            }
+            res.json(recordset)
+        })
 })
 
 app.listen(3001, () => {
